c02720683b1498802e6feb88590c2d16c31cdd7a
chetanchandel31 <chetanchandel3@gmail.com>
2021-04-23 20:15:02 +0530

    add debounce

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,13 +1,31 @@
-import { withStyles } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import useStyles from "./styles";
 
 const Editor = () => {
+	const [text, setText] = useState("");
+	const [inputChangeTracker, setInputChangeTracker] = useState(0);
 	const classes = useStyles();
+
+	useEffect(() => {
+		//come back later
+		let timeout = setTimeout(() => console.log("hi"), 1500);
+
+		return () => clearTimeout(timeout);
+	}, [inputChangeTracker]);
+
+	const updateBody = val => {
+		setText(val);
+		update();
+	};
+
+	const update = () => {
+		setInputChangeTracker(prevState => prevState + 1);
+	};
 
 	return (
 		<div className={classes.editorContainer}>
-			<ReactQuill />
+			<ReactQuill value={text} onChange={updateBody} />
 		</div>
 	);
 };
