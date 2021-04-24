87c1e08ad903ef6ced1ac005ce644c3ca5fb23b9
chetanchandel31 <chetanchandel3@gmail.com>
2021-04-24 16:29:28 +0530

    add sidebar item component's content

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,6 @@
-import { Button } from "@material-ui/core";
+import { Button, Divider, List } from "@material-ui/core";
 import { useState } from "react";
+import SidebarItem from "./sidebaritem/SidebarItem";
 import useStyles from "./styles";
 
 const Sidebar = ({ notes, selectedNoteIndex }) => {
@@ -17,16 +18,41 @@ const Sidebar = ({ notes, selectedNoteIndex }) => {
 		setTitle(text);
 	};
 
+	const submitNoteHandler = () => {
+		console.log(addingNote, title);
+	};
+
+	const selectNote = () => {
+		console.log("select note");
+	};
+
+	const deleteNote = () => {
+		console.log("delete note");
+	};
+
 	return (
 		<div className={classes.sidebarContainer}>
 			<Button className={classes.newNoteBtn} onClick={newNoteBtnClick}>
 				New note
 			</Button>
+
 			{addingNote && (
 				<div>
 					<input type="text" className={classes.newNoteInput} placeholder="Enter note title" onChange={({ target }) => updateTitle(target.value)} />
+					<Button className={classes.newNoteSubmitBtn} onClick={submitNoteHandler}>
+						Submit note
+					</Button>
 				</div>
 			)}
+
+			<List>
+				{notes?.map((_note, _index) => (
+					<div key={_index}>
+						<SidebarItem _note={_note} _index={_index} selectedNoteIndex={selectedNoteIndex} selectNote={selectNote} deleteNote={deleteNote} />
+						<Divider />
+					</div>
+				))}
+			</List>
 		</div>
 	);
 };
--- src/components/sidebar/sidebaritem/SidebarItem.js
+++ src/components/sidebar/sidebaritem/SidebarItem.js
@@ -1,8 +1,26 @@
-import { withStyles } from "@material-ui/core";
-import styles from "./styles";
+import { ListItem, ListItemText } from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+import { removeHTMLTags } from "../../../helpers/helpers";
+import useStyles from "./styles";
 
-const SidebarItem = () => {
-	return <div>sidebar item</div>;
+const SidebarItem = ({ _index, _note, selectedNoteIndex, selectNote, deleteNote }) => {
+	const classes = useStyles();
+
+	//try to implement confirm in parent comp only
+	const deleteHandler = note => {
+		if (window.confirm(`Are you sure you want to delete ${note.title}`)) deleteNote(note);
+	};
+
+	return (
+		<div key={_index}>
+			<ListItem className={classes.listItem} selected={_index === selectedNoteIndex} alignItems="flex-start">
+				<div className={classes.textSection} onClick={() => selectNote(_note, _index)}>
+					<ListItemText primary={_note.title} secondary={removeHTMLTags(_note.body)} />
+				</div>
+			</ListItem>
+			<Delete className={classes.deleteIcon} onClick={() => deleteHandler(_note)} />
+		</div>
+	);
 };
 
-export default withStyles(styles)(SidebarItem);
+export default SidebarItem;
