d8f5f8498b72eb982a6c4a2da3104de35d408646
chetanchandel31 <chetanchandel3@gmail.com>
2021-08-16 19:08:11 +0530

    smarter sidebar text

--- src/components/sidebar/sidebaritem/SidebarItem.js
+++ src/components/sidebar/sidebaritem/SidebarItem.js
@@ -14,7 +14,10 @@ const SidebarItem = ({ _index, _note, selectedNoteIndex, selectNote, deleteNote 
 		<div key={_index}>
 			<ListItem className={classes.listItem} selected={_index === selectedNoteIndex} alignItems="flex-start">
 				<div className={classes.textSection} onClick={() => selectNote(_note, _index)}>
-					<ListItemText primary={_note.title} secondary={removeHTMLTags(_note.body.slice(0, 30) + "...")} />
+					<ListItemText
+						primary={`${_note.title.slice(0, 23)}${_note.title.length > 23 ? "..." : ""}`}
+						secondary={`${removeHTMLTags(_note.body.slice(0, 30))}${_note.body.length > 30 ? "..." : ""}`}
+					/>
 				</div>
 				<Delete className={classes.deleteIcon} onClick={() => deleteHandler(_note)} />
 			</ListItem>
